b2463a6dcaba51ed334aabf73e0e1e205828baeb
Sandeep Bhattarai <sandeepbhattarai82@gmail.com>
2022-09-22 20:18:43 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const app = () => {
+  return (
+    <div className='w-screen h-screen flex items-center justify-center text-blue-600'>app for food is good mood</div>
+  )
+}
+
+export default app;
